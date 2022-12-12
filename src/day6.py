a1da6a3c34280ee30f9c8f254f11db4f578bd4fd
Henna Lehtinen <henna@iontau.com>
2022-12-12 17:35:53 +0200

    Added day6b

--- src/day6.py
+++ src/day6.py
@@ -17,4 +17,25 @@ def day6a(filepath):
                     break 
     return char_count
 
-print(day6a('day6_veronica.txt'))
+
+def day6b(filepath):
+    char_count=0
+    with open(filepath) as f:
+        for line in f:
+            length = len(line)
+            found = False
+            for n in range(0,length-14+1):
+                for x in range(n,n+14):
+                    el = line[x]
+                    if line[n:n+14].count(el)>1:
+                        break
+                    elif x == n+13 and line[n:n+14].count(el)==1:
+                        found=True
+                        char_count += n+13+1 
+                if found:
+                    break 
+    return char_count
+
+#print(day6a('day6_veronica.txt'))
+#print(day6a('henna_day6.txt'))
+print(day6b('henna_day6.txt'))
